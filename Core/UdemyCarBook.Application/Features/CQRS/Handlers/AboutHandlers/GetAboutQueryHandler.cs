5a4dac3a871b98bd9c00eb8cb317b098a342357e
arenumutcesmeci <arenumutcesmeci@gmail.com>
2024-03-17 17:17:00 +0300

    Add GetAboutQueryHandler and GetAboutQueryHandler class

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _aboutRepository;
+
+        public GetAboutQueryHandler(IRepository<About> aboutRepository)
+        {
+            _aboutRepository = aboutRepository;
+        }
+
+        public async Task<List<GetAboutByIdQueryResult>> Handle()
+        {
+            var values = await _aboutRepository.GetAllAsync();
+            return values.Select(x=> new GetAboutByIdQueryResult
+            {
+                AboutId = x.AboutId,
+                Description = x.Description,
+                Title = x.Title,
+                ImageUrl=x.ImageUrl
+            }).ToList();
+        }
+
+    }
+}
